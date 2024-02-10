abf549f31cd64fd01cf32bb34083063822dd2dee
john-mirage <28307090+john-mirage@users.noreply.github.com>
2024-02-10 16:10:05 +0100

    feat: initial commit

---
+++ src/components/App.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Card from "./Card.svelte";
+</script>
+
+<main class="p-24 min-h-screen flex justify-center items-center bg-yellow">
+    <h1 class="sr-only">Blog preview card</h1>
+    <Card/>
+</main>
---
+++ tailwind.config.js
@@ -0,0 +1,110 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+const pxToRem = (px) => {
+  if (typeof px === "number") {
+    return `${String(px / 16)}rem`;
+  }
+  throw new Error("The pixels value must be a number");
+}
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{svelte,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      "xs": "432px",
+      "hover-device": { "raw": "(hover: hover)" },
+    },
+    colors: {
+      "transparent": "transparent",
+      "yellow": "#F4D04E",
+      "white": "#FFFFFF",
+      "black": "#111111",
+      "grey": "#6C6C6C",
+    },
+    spacing: {
+      "0": "0px",
+      "1": pxToRem(1),
+      "4": pxToRem(4),
+      "12": pxToRem(12),
+      "14": pxToRem(14),
+      "18": pxToRem(18),
+      "20": pxToRem(20),
+      "21": pxToRem(21),
+      "24": pxToRem(24),
+      "30": pxToRem(30),
+      "32": pxToRem(32),
+      "384": pxToRem(384),
+    },
+    boxShadow: {
+      "card": "8px 8px 0px 0px #000",
+    },
+    borderRadius: {
+      "image": pxToRem(10),
+      "card": pxToRem(20),
+      "tag": pxToRem(4),
+    },
+    fontSize: ({ theme }) => ({
+      "tag-sm": [theme("spacing[12]"), {
+        lineHeight: theme("spacing[18]"),
+        letterSpacing: "0",
+        fontWeight: "800",
+      }],
+      "tag-lg": [theme("spacing[14]"), {
+        lineHeight: theme("spacing[21]"),
+        letterSpacing: "0",
+        fontWeight: "800",
+      }],
+      "date-sm": [theme("spacing[12]"), {
+        lineHeight: theme("spacing[18]"),
+        letterSpacing: "0",
+        fontWeight: "500",
+      }],
+      "date-lg": [theme("spacing[14]"), {
+        lineHeight: theme("spacing[21]"),
+        letterSpacing: "0",
+        fontWeight: "500",
+      }],
+      "title-sm": [theme("spacing[20]"), {
+        lineHeight: theme("spacing[30]"),
+        letterSpacing: "0",
+        fontWeight: "800",
+      }],
+      "title-lg": [theme("spacing[24]"), {
+        lineHeight: theme("spacing[36]"),
+        letterSpacing: "0",
+        fontWeight: "800",
+      }],
+      "description-sm": [theme("spacing[14]"), {
+        lineHeight: theme("spacing[21]"),
+        letterSpacing: "0",
+        fontWeight: "500",
+      }],
+      "description-lg": [theme("spacing[16]"), {
+        lineHeight: theme("spacing[24]"),
+        letterSpacing: "0",
+        fontWeight: "500",
+      }],
+      "author-sm": [theme("spacing[14]"), {
+        lineHeight: theme("spacing[21]"),
+        letterSpacing: "0",
+        fontWeight: "800",
+      }],
+      "author-lg": [theme("spacing[14]"), {
+        lineHeight: theme("spacing[21]"),
+        letterSpacing: "0",
+        fontWeight: "800",
+      }],
+    }),
+    extend: {
+      fontFamily: {
+        sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
+
